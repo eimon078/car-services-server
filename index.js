b2a0e70d1fd224bd187b2837e6e11c4c26e4ee7a
taief078 <eimonhossain078@gmail.com>
2021-10-27 15:58:59 +0600

    car services server

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
+const app = express();
+const port = process.env.Port || 7000;
+
+app.use(cors());
+app.use(express.json());
+
+//Mondodb connention
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nxj01.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('carMechanic');
+        const servicesCollection = database.collection('services');
+
+        //get api
+
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        //get single value
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            console.log(id, query);
+            const service = await servicesCollection.findOne(query);
+            console.log(service);
+            res.json(service);
+        })
+
+        //Post Api
+        app.post('/services', async (req, res) => {
+            const service = req.body
+            console.log('post hiting', service);
+
+
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
+
+        })
+
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send("Car Server");
+})
+
+app.listen(port, () => {
+    console.log('listing port', port);
+})
